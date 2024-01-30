8be09d0287836bffeff5f61027bfd8952b803cc9
cti1650 <cti1650@gmail.com>
2024-01-30 09:33:07 +0000

    feat: update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -4,9 +4,8 @@ import Config
 config :myapp, Myapp.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "myapp_dev",
-  stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
@@ -19,7 +18,7 @@ config :myapp, Myapp.Repo,
 config :myapp, MyappWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 1}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
